5bcbf236512aa128c1d09f0d25bace6f4e335db8
jcanny <jfc@cs.berkeley.edu>
2012-10-19 14:53:29 -0700

    added bidmat.cmd

---
+++ bidmat.cmd
@@ -0,0 +1,17 @@
+@ECHO OFF
+:: Set JAVA_HOME here if not set in environment
+:: SET JAVA_HOME= 
+:: Set as much memory as possible
+(SET JAVA_OPTS=-Xmx6G -Xms128M)
+:: Fix these if needed
+SET JCUDA_VERSION=0.4.2
+SET JCUDA_LIBDIR=%CD%\lib
+SET LIBDIR=%CD%\lib
+
+SET BIDMAT_LIBS=%CD%\BIDMat.jar;%LIBDIR%\ptplot.jar;%LIBDIR%\ptplotapplication.jar;%LIBDIR%\jhdf5.jar
+
+SET JCUDA_LIBS=%JCUDA_LIBDIR%\jcuda-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcublas-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcufft-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcurand-%JCUDA_VERSION%.jar;%JCUDA_LIBDIR%\jcusparse-%JCUDA_VERSION%.jar
+
+SET ALL_LIBS=%BIDMAT_LIBS%;%JCUDA_LIBS%;%JAVA_HOME%\lib\tools.jar
+
+scala -nobootcp -cp "%ALL_LIBS%" -Yrepl-sync -i %LIBDIR%\bidmat_init.scala
