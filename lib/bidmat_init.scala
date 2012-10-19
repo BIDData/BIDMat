b0dcfe660000bdb942ebbcc19b7c1f98bb5f073a
jcanny <jfc@cs.berkeley.edu>
2012-10-19 14:55:31 -0700

    added bidmat_init.scala

---
+++ lib/bidmat_init.scala
@@ -0,0 +1,13 @@
+import BIDMat.{Mat, DMat, FMat, IMat, BMat, CMat, SMat, SDMat, GMat, GSMat, HMat}
+import BIDMat.MatFunctions._
+import BIDMat.SciFunctions._
+import BIDMat.Solvers._
+import BIDMat.Plotting._
+
+{
+var a = new Array[Int](1)
+jcuda.runtime.JCuda.cudaGetDeviceCount(a)
+if (a(0) > 0) jcuda.runtime.JCuda.initialize
+Mat.hasCUDA = a(0)
+}
+printf("%d CUDA device%s found\n", Mat.hasCUDA, if (Mat.hasCUDA == 1) "" else "s")
