d77b1d99f457ab435a192ede4b0f821e91f96441
jcanny <jfc@cs.berkeley.edu>
2012-10-19 11:33:31 -0700

    improving build and run scripts

--- build.sbt
+++ build.sbt
@@ -5,51 +5,19 @@ version := "0.1.0"
 
 organization := "edu.berkeley.bid"
 
-scalaVersion := "2.9.1"
-
-crossScalaVersions := Seq("2.8.2", "2.9.1")
+scalaVersion := "2.9.2"
 
 resolvers ++= Seq(
-  "ScalaNLP Maven2" at "http://repo.scalanlp.org/repo",
   "Scala Tools Snapshots" at "http://scala-tools.org/repo-snapshots/"
-)
-
-libraryDependencies ++= Seq(
-  "jfree" % "jcommon" % "1.0.16",
-  "jfree" % "jfreechart" % "1.0.13",
-//  "org.apache.xmlgraphics" % "xmlgraphics-commons" % "1.3.1", // for eps gen
-  // "org.apache.xmlgraphics" % "batik-dom" % "1.7",    // for svg gen
-  // "org.apache.xmlgraphics" % "batik-svggen" % "1.7", // for svg gen
-//  "com.lowagie" % "itext" % "2.1.5" intransitive(),  // for pdf gen
-  "junit" % "junit" % "4.5" % "test"
 )
 
 libraryDependencies <<= (scalaVersion, libraryDependencies) { (sv, deps) =>
   deps :+ ("org.scala-lang" % "scala-compiler" % sv)
 }
 
-libraryDependencies <<= (scalaVersion, libraryDependencies) { (sv, deps) =>
-  sv match {
-    case "2.9.1" =>
-      (deps :+ ("jline" % "jline" % "0.9.94") 
-             :+ ("org.scala-lang" % "jline" % "2.9.1"))
-    case x if x.startsWith("2.8") =>
-      deps :+ ("jline" % "jline" % "0.9.94")
-    case x       => error("Unsupported Scala version " + x)
-  }
-}
+libraryDependencies += "org.scala-lang" % "jline" % "2.9.2"
 
-libraryDependencies <<= (scalaVersion, libraryDependencies) { (sv, deps) =>
-  sv match {
-    case "2.9.1" =>
-      (deps :+ ("org.scalatest" % "scalatest" % "1.4.RC2" % "test")
-            :+ ("org.scala-tools.testing" %% "scalacheck" % "1.9" % "test"))
-    case x if x.startsWith("2.8") =>
-      (deps :+ ("org.scalatest" % "scalatest" % "1.3" % "test")
-            :+ ("org.scala-tools.testing" % "scalacheck_2.8.1" % "1.8" % "test"))
-    case x  => error("Unsupported Scala version " + x)
-  }
-}
+libraryDependencies += "org.scalatest" %% "scalatest" % "1.8" % "test"
 
 credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
 
---
+++ src/main/scala/BIDMat/Plotting.scala
@@ -0,0 +1,89 @@
+package BIDMat
+import ptolemy.plot._
+
+object Plotting { 
+  var ifigure:Int = 1
+  
+  def plot(mats:Mat*)(xlog:Boolean=false, ylog:Boolean=false, isconnected:Boolean=true) = {
+    var p:Plot = new Plot
+    p.setXLog(xlog)
+    p.setYLog(ylog)
+    val dataset = 0
+    if (mats.length == 1) {
+      val m = mats(0)
+      if (m.nrows == 1 || m.ncols == 1) { 
+      	m match { 
+      	case mf:FMat => for (i <- 0 until m.length) p.addPoint(dataset, i, mf(i), isconnected)
+      	case md:DMat => for (i <- 0 until m.length) p.addPoint(dataset, i, md(i), isconnected)
+      	case mi:IMat => for (i <- 0 until m.length) p.addPoint(dataset, i, mi(i), isconnected)
+      	}
+      } else {
+      	for (i <- 0 until m.ncols) {
+      		m match { 
+      		case mf:FMat => for (j <- 0 until m.nrows) p.addPoint(i, j, mf(j,i), isconnected)
+      		case md:DMat => for (j <- 0 until m.nrows) p.addPoint(i, j, md(j,i), isconnected)
+      		case mi:IMat => for (j <- 0 until m.nrows) p.addPoint(i, j, mi(j,i), isconnected)
+      		}     
+      	}
+      }
+    } else {
+      var i = 0
+    	while (i*2 <= mats.length) {      
+    		(mats(2*i), mats(2*i+1)) match { 
+    		case (a:FMat, b:FMat) => for (j <- 0 until a.length) p.addPoint(i, a(j), b(j), isconnected)
+    		case (a:FMat, b:DMat) => for (j <- 0 until a.length) p.addPoint(i, a(j), b(j), isconnected)
+    		case (a:DMat, b:FMat) => for (j <- 0 until a.length) p.addPoint(i, a(j), b(j), isconnected)
+    		case (a:DMat, b:DMat) => for (j <- 0 until a.length) p.addPoint(i, a(j), b(j), isconnected)
+    		case (a:FMat, b:IMat) => for (j <- 0 until a.length) p.addPoint(i, a(j), b(j), isconnected)
+    		case (a:DMat, b:IMat) => for (j <- 0 until a.length) p.addPoint(i, a(j), b(j), isconnected)
+    		case (a:IMat, b:FMat) => for (j <- 0 until a.length) p.addPoint(i, a(j), b(j), isconnected)
+    		case (a:IMat, b:DMat) => for (j <- 0 until a.length) p.addPoint(i, a(j), b(j), isconnected)
+    		case (a:IMat, b:IMat) => for (j <- 0 until a.length) p.addPoint(i, a(j), b(j), isconnected)
+    		}  
+    		i += 1
+    	}
+    }
+    var pframe:PlotFrame = new PlotFrame("Figure "+ifigure, p)
+    ifigure += 1
+    pframe.setVisible(true)
+  }
+  
+  def loglog(mats:Mat*) = plot(mats: _*)(xlog=true, ylog=true)
+  
+  def semilogx(mats:Mat*) = plot(mats: _*)(xlog=true)
+  
+  def semilogy(mats:Mat*) = plot(mats: _*)(ylog=true)
+   
+
+  
+  def hist(m:Mat, nbars:Int=10) = { 
+    import SciFunctions._
+    var p:Histogram = new Histogram
+    val dataset = 0
+    if (m.nrows == 1 || m.ncols == 1) { 
+    	m match { 
+    	case mf:FMat => {
+    	  var vmax = maxi(mf,0).v
+    	  var vmin = mini(mf,0).v
+    	  p.setBinWidth((vmax-vmin)/nbars)
+    	  for (i <- 0 until m.length) p.addPoint(dataset, mf(i))
+    	}
+    	case md:DMat => {
+    		var vmax = maxi(md,0).v
+    	  var vmin = mini(md,0).v
+    	  p.setBinWidth((vmax-vmin)/nbars)
+    	  for (i <- 0 until m.length) p.addPoint(dataset, md(i))
+    	}
+    	case mi:IMat => {
+    		var vmax = maxi(mi,0).v.asInstanceOf[Double]
+    	  var vmin = mini(mi,0).v
+    	  p.setBinWidth((vmax-vmin)/nbars)
+    	  for (i <- 0 until m.length) p.addPoint(dataset, mi(i))
+    	}
+      }
+    }
+    var pframe:PlotFrame = new PlotFrame("Figure "+ifigure, p)
+    ifigure += 1
+    pframe.setVisible(true)
+  }
+}
