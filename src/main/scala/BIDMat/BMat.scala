523b5534940d1993da7d57552d34a973a8cfb4ab
jcanny <jfc@cs.berkeley.edu>
2012-10-27 13:09:13 -0700

    added CUDA 5 jars

--- src/main/scala/BIDMat/BMat.scala
+++ src/main/scala/BIDMat/BMat.scala
@@ -2,7 +2,7 @@ package BIDMat
 import edu.berkeley.bid.CBLAS._
 import edu.berkeley.bid.LAPACK._
 
-case class BMat(nr:Int, nc:Int, data0:Array[Byte]) extends DenseMat[Byte](nr, nc, data0) {
+case class BMat(nr:Int, nc:Int, nnz1:Int, ir0:Array[Int], jc0:Array[Int], data0:Array[Byte]) extends SparseMat[Byte](nr, nc, nnz1, ir0, jc0, data0) {
 
   def size() = length;
   
@@ -24,108 +24,75 @@ case class BMat(nr:Int, nc:Int, data0:Array[Byte]) extends DenseMat[Byte](nr, nc
   
   override def mytype = "BMat"
       
-  override def t:BMat = BMat(gt(null))
+  override def t:BMat = BMat(gt)
   
-  def horzcat(b: BMat) = BMat(ghorzcat(b))
+  def horzcat(b: BMat) = BMat(super.horzcat(b))
   
-  def vertcat(b: BMat) = BMat(gvertcat(b))
+  def vertcat(b: BMat) = BMat(super.vertcat(b))
   
-  def find3:(IMat, IMat, BMat) = { val (ii, jj, vv) = gfind3 ; (IMat(ii), IMat(jj), BMat(vv)) }
-  
-  override def apply(a:IMat):BMat = BMat(gapply(a))
+  def find3:(IMat, IMat, IMat) = { 
+    val (ii, jj, vv) = gfind3 
+    val vi = IMat(vv.length, 1)
+    Mat.copyToIntArray(vv.data, 0, vi.data, 0, vv.length)
+    (IMat(ii), IMat(jj), vi)
+  }
   
   override def apply(a:IMat, b:IMat):BMat = BMat(gapply(a, b))	
   
-  override def apply(a:IMat, b:Int):BMat = BMat(gapply(a, b))	
+  override def apply(a:IMat, b:Int):BMat = BMat(gapply(a, IMat.ielem(b)))	
   
-  override def apply(a:Int, b:IMat):BMat = BMat(gapply(a, b))
+  override def apply(a:Int, b:IMat):BMat = BMat(gapply(IMat.ielem(a), b))
   
-  def bbMatOp(b: Mat, f:(Byte, Byte) => Byte, out:Mat):BMat = 
-    b match {
-      case bb:BMat => BMat(ggMatOp(bb, f, tryForOutBMat(out)))
-      case _ => throw new RuntimeException("unsupported operation "+f+" on "+this+" and "+b)	
+  def bbMatOp(b: BMat, f:(Byte, Byte) => Byte, out:Mat):BMat = BMat(sgMatOp(b, f))
+  
+  def bbMatOpScalar(b: Byte, f:(Byte, Byte) => Byte, out:Mat):BMat = BMat(sgMatOpScalar(b, f))
+  
+  def bbReduceOp(n:Int, f1:(Byte) => Byte, f2:(Byte, Byte) => Byte) = IMat(sgReduceOp(n, f1, f2))
+  
+  def toCSMat:CSMat = {
+    val out = CSMat(ncols, 1)
+    val ioff = Mat.ioneBased
+    var i = 0
+    while (i < ncols) {
+      out.data(i) = new String(data, jc(i)-ioff, jc(i+1)-jc(i), BMat.encoding)
+      i += 1
     }
-  
-  def bbMatOpv(b: Mat, f:(Array[Byte],Int,Int,Array[Byte],Int,Int,Array[Byte],Int,Int,Int) => Byte, out:Mat) = 
-    b match {
-      case bb:BMat => BMat(ggMatOpv(bb, f, tryForOutBMat(out)))
-      case _ => throw new RuntimeException("unsupported operation "+f+" on "+this+" and "+b)	
-    }
-  
-  def bbMatOpScalar(b: Byte, f:(Byte, Byte) => Byte, out:Mat):BMat = BMat(ggMatOpScalar(b, f, tryForOutBMat(out)))
-  
-  def bbMatOpScalarv(b: Byte, f:(Array[Byte],Int,Int,Array[Byte],Int,Int,Array[Byte],Int,Int,Int) => Byte, out:Mat) = 
-    BMat(ggMatOpScalarv(b, f, tryForOutBMat(out)))
-  
-  def bbReduceOp(n:Int, f1:(Byte) => Byte, f2:(Byte, Byte) => Byte, out:Mat) = 
-    BMat(ggReduceOp(n, f1, f2, tryForOutBMat(out)))
-  
-  def bbReduceOpv(n:Int, f:(Array[Byte],Int,Int,Array[Byte],Int,Int,Array[Byte],Int,Int,Int) => Byte, out:Mat) = 
-    BMat(ggReduceOpv(n, f, tryForOutBMat(out)))
-  
-  def bbReduceAll(n:Int, f1:(Byte) => Byte, f2:(Byte, Byte) => Byte, out:Mat) = 
-    BMat(ggReduceAll(n, f1, f2, tryForOutBMat(out)))
-  
-  def bbReduceAllv(n:Int, f:(Array[Byte],Int,Int,Array[Byte],Int,Int,Array[Byte],Int,Int,Int) => Byte, out:Mat) = 
-    BMat(ggReduceAllv(n, f, tryForOutBMat(out)))
-  
-  override def printOne(i:Int):String = { 
-    val v = data(i)
-    "%d" format v.intValue
+    out
   }
   
+  override def toString:String = { 
+  	val somespaces = "                                                               "
+  	val ioff = Mat.ioneBased
+  	val ss = new StringBuilder
+  	val nChars = Mat.terminalWidth-4
+  	val totchars = 10*nChars
+  	var nelems = 0
+  	var maxlen = 0
+  	val lbuf = new scala.collection.mutable.ListBuffer[String]
+  	while (maxlen * nelems < totchars && nelems < ncols) {
+  	  val str = new String(data, jc(nelems)-ioff, jc(nelems+1)-jc(nelems), BMat.encoding)
+  	  lbuf.append(str)
+  	  maxlen = math.max(maxlen, 1+str.length)
+  	  nelems += 1
+  	}
+  	nelems -= 1
+  	var i = 0
+  	var thisrow = 0
+  	lbuf.forall((str:String) => {
+  		ss.append(str + somespaces.substring(0, maxlen - str.length))
+  	  thisrow += 1
+  	  if ((thisrow + 1) * maxlen >= nChars) {
+  	    ss.append("\n")
+  	    thisrow = 0
+  	  }
+  		true
+  	})
+  	if (nelems < ncols) {
+  		ss.append("...")
+  	}
+  	ss.toString
+  }
     
-  override def clearUpper(off:Int) = setUpper(0, off)
-  override def clearUpper = setUpper(0, 0)
-  
-  override def clearLower(off:Int) = setLower(0, off)
-  override def clearLower = setLower(0, 0)
-
- /*
-  * Column-based (Streaming) multiply
-  */
-  
-  def DMult(a:Mat):BMat = 
-    a match {
-      case aa:BMat => {
-	if (ncols == a.nrows) {
-	  val out = BMat(nrows, a.ncols) // Needs to be cleared
-	  for (i <- 0 until a.ncols)
-	    for (j <- 0 until a.nrows) {
-	      var k = 0
-	      val dval = aa.data(j + i*ncols)
-	      while (k < nrows) {
-		out.data(k+i*nrows) = (out.data(k+i*nrows)+data(k+j*nrows)*dval).asInstanceOf[Byte]
-		k += 1
-	      }
-	    }
-	  out
-	} else throw new RuntimeException("dimensions mismatch")
-      }
-      case _ => throw new RuntimeException("argument must be dense")
-    }
-  
-  
-  
-  override def + (b : Mat) = bbMatOpv(b, DenseMat.vecAdd _, null)
-  override def - (b : Mat) = bbMatOpv(b, DenseMat.vecSub _, null)
-  override def * (b : Mat) = DMult(b)
-  override def *@ (b : Mat) = bbMatOpv(b, DenseMat.vecMul _, null)
-  
-  def * (b : Byte) = DMult(BMat.belem(b))
-  def + (b : Byte) = bbMatOpScalarv(b, DenseMat.vecAdd _, null)
-  def - (b : Byte) = bbMatOpScalarv(b, DenseMat.vecSub _, null)
-  def *@ (b : Byte) = bbMatOpScalarv(b, DenseMat.vecMul _, null)
-
-  
-  override def > (b : Mat) = bbMatOp(b, (x:Byte, y:Byte) => if (x > y) 1 else 0, null)
-  override def < (b : Mat) = bbMatOp(b, (x:Byte, y:Byte) => if (x < y) 1 else 0, null)
-  override def == (b : Mat) = bbMatOp(b, (x:Byte, y:Byte) => if (x == y) 1 else 0, null)
-  override def === (b : Mat) = bbMatOp(b, (x:Byte, y:Byte) => if (x == y) 1 else 0, null)
-  override def >= (b : Mat) = bbMatOp(b, (x:Byte, y:Byte) => if (x >= y) 1 else 0, null)
-  override def <= (b : Mat) = bbMatOp(b, (x:Byte, y:Byte) => if (x <= y) 1 else 0, null)
-  override def != (b : Mat) = bbMatOp(b, (x:Byte, y:Byte) => if (x != y) 1 else 0, null)
-
   def > (b : Byte) = bbMatOpScalar(b, (x:Byte, y:Byte) => if (x > y) 1 else 0, null)
   def < (b : Byte) = bbMatOpScalar(b, (x:Byte, y:Byte) => if (x < y) 1 else 0, null)
   def == (b : Byte) = bbMatOpScalar(b, (x:Byte, y:Byte) => if (x == y) 1 else 0, null)
@@ -137,12 +104,10 @@ case class BMat(nr:Int, nc:Int, data0:Array[Byte]) extends DenseMat[Byte](nr, nc
   override def \ (b: Mat) = b match {
     case fb:BMat => horzcat(fb)
   }
-  def \ (b: Byte) = horzcat(BMat.belem(b))
   
   override def on (b: Mat) = b match {
     case fb:BMat => vertcat(fb)
   }
-  def on (b: Byte) = vertcat(BMat.belem(b))
   
   override def ~ (b: Mat):Pair = 
     b match {
@@ -153,26 +118,7 @@ case class BMat(nr:Int, nc:Int, data0:Array[Byte]) extends DenseMat[Byte](nr, nc
 
 class BPair (val omat:Mat, val mat:BMat) extends Pair {
   
-  override def t:BMat = BMat(mat.gt(mat.tryForOutBMat(omat)))
-  
-  override def * (b : Mat) = mat.DMult(b)  
-  override def + (b : Mat) = mat.bbMatOpv(b, DenseMat.vecAdd _, omat)
-  override def - (b : Mat) = mat.bbMatOpv(b, DenseMat.vecSub _, omat)
-  override def *@ (b : Mat) = mat.bbMatOpv(b, DenseMat.vecMul _, omat)
-
-  override def > (b : Mat) = mat.bbMatOp(b, (x:Byte, y:Byte) => if (x > y) 1 else 0, omat)
-  override def < (b : Mat) = mat.bbMatOp(b, (x:Byte, y:Byte) => if (x < y) 1 else 0, omat)
-  override def == (b : Mat) = mat.bbMatOp(b, (x:Byte, y:Byte) => if (x == y) 1 else 0, omat)
-  override def === (b : Mat) = mat.bbMatOp(b, (x:Byte, y:Byte) => if (x == y) 1 else 0, omat)
-  override def >= (b : Mat) = mat.bbMatOp(b, (x:Byte, y:Byte) => if (x >= y) 1 else 0, omat)
-  override def <= (b : Mat) = mat.bbMatOp(b, (x:Byte, y:Byte) => if (x <= y) 1 else 0, omat)
-  override def != (b : Mat) = mat.bbMatOp(b, (x:Byte, y:Byte) => if (x != y) 1 else 0, omat) 
-  
-  def * (b : Byte) = mat.DMult(BMat.belem(b))
-  def + (b : Byte) = mat.bbMatOpScalarv(b, DenseMat.vecAdd _, null)
-  def - (b : Byte) = mat.bbMatOpScalarv(b, DenseMat.vecSub _, null)
-  def *@ (b : Byte) = mat.bbMatOpScalarv(b, DenseMat.vecMul _, null)
-
+ 
   def > (b : Byte) = mat.bbMatOpScalar(b, (x:Byte, y:Byte) => if (x > y) 1 else 0, omat)
   def < (b : Byte) = mat.bbMatOpScalar(b, (x:Byte, y:Byte) => if (x < y) 1 else 0, omat)
   def == (b : Byte) = mat.bbMatOpScalar(b, (x:Byte, y:Byte) => if (x == y) 1 else 0, omat)
@@ -183,35 +129,39 @@ class BPair (val omat:Mat, val mat:BMat) extends Pair {
 
 object BMat {
   
-  def apply(nr:Int, nc:Int) = new BMat(nr, nc, new Array[Byte](nr*nc))
+  def apply(nr:Int, nc:Int, nnz0:Int):BMat = new BMat(nr, nc, nnz0, new Array[Int](nnz0), new Array[Int](nc+1), new Array[Byte](nnz0)) 
   
-  def apply(a:DenseMat[Byte]):BMat = new BMat(a.nrows, a.ncols, a.data) 
-
-  def apply(x:Mat):BMat = {
-    val out = BMat(x.nrows, x.ncols)
-    x match {
-      case ff:BMat => System.arraycopy(ff.data, 0, out.data, 0, ff.length)
-      case _ => throw new RuntimeException("Unsupported source type")
-    }
-    out
-  }
-
-  def belem(x:Byte) = {
-    val out = BMat(1,1)
-    out.data(0) = x
-    out
-  }
+  def apply(a:SparseMat[Byte]):BMat = new BMat(a.nrows, a.ncols, a.nnz, a.ir, a.jc, a.data) 
+   
+  def SnoRows(nr:Int, nc:Int, nnz0:Int):BMat = new BMat(nr, nc, nnz0, null, new Array[Int](nc+1), new Array[Byte](nnz0))
   
-  def newOrCheckBMat(nr:Int, nc:Int, outmat:BMat):BMat = {
-    if (outmat.asInstanceOf[AnyRef] == null) {
-      BMat(nr, nc)
-    } else {
-      if (outmat.nrows != nr || outmat.ncols != nc) {
-        throw new RuntimeException("dimensions mismatch")
-      } else {
-      	outmat
-      }
+  var encoding = "UTF8"
+//  	var encoding = "UTF_16LE"
+  
+  def apply(cc:CSMat):BMat = {
+    val ioff = Mat.ioneBased
+    val ncolsx = cc.length
+    var nrowsx = 0
+    var nnzx = 0
+    var i = 0
+    while (i < ncolsx) {
+      val len = cc(i).getBytes(encoding).length
+      nnzx += len
+      nrowsx = math.max(nrowsx, 1+len)
+      i += 1
     }
+    val out = SnoRows(nrowsx, ncolsx, nnzx)
+    nnzx = 0
+    i = 0
+    while (i < ncolsx) {
+      out.jc(i) = nnzx + ioff
+      val bytes = cc(i).getBytes(encoding)
+      System.arraycopy(bytes, 0, out.data, nnzx, bytes.length)
+      nnzx += bytes.length
+      i += 1
+    } 
+    out.jc(i) = nnzx
+    out
   }
 }
 
