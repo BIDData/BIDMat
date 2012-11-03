e7f57f22eb9e51507e65bd373defd49f43eb5157
jcanny <jfc@cs.berkeley.edu>
2012-11-03 15:21:36 -0700

    trying szip compression

--- src/main/scala/BIDMat/MatHDF5.scala
+++ src/main/scala/BIDMat/MatHDF5.scala
@@ -13,7 +13,8 @@ object MatHDF5 {
 		  val cdims = new Array[Long](1)
 		  cdims(0) = math.max(1, math.min(dims(0), Mat.compressedBlock))
 		  H5Pset_chunk(dplist_id, 1, cdims)
-		  H5Pset_deflate(dplist_id, Mat.compressionLevel)
+//		  H5Pset_deflate(dplist_id, Mat.compressionLevel)
+		  H5Pset_szip(dplist_id, 	H5_SZIP_EC_OPTION_MASK, 32)
 		}
 	  } else {
 		if (dims(0)*dims(1) > 1024) {
@@ -21,7 +22,8 @@ object MatHDF5 {
 		  cdims(0) = math.max(1, math.min(dims(0), 1+Mat.compressedBlock/dims(1)))
 		  cdims(1) = math.max(1, dims(1))
 		  H5Pset_chunk(dplist_id, 2, cdims)
-		  H5Pset_deflate(dplist_id, Mat.compressionLevel)
+//		  H5Pset_deflate(dplist_id, Mat.compressionLevel)
+		  H5Pset_szip(dplist_id, 	H5_SZIP_EC_OPTION_MASK, 32)
 		}
 	  }
 	}
