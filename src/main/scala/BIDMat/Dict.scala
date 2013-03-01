ccd7ba8020f2088d3f2efe0fd38c0b63fd12d991
jcanny <jfc@cs.berkeley.edu>
2013-03-01 10:02:36 -0800

    added Dict.scala

---
+++ src/main/scala/BIDMat/Dict.scala
@@ -0,0 +1,139 @@
+package BIDMat
+import scala.collection.mutable.HashMap
+import MatFunctions._
+
+class Dict(val cstr:CSMat) { 
+
+  val length = cstr.length
+  
+  var counts:IMat = null
+  
+  var hash:HashMap[String,Int] = null
+
+  def makeHash:HashMap[String, Int] = { 
+    if (hash.asInstanceOf[AnyRef] == null) { 
+      hash = new HashMap[String, Int]()
+      var i = 0
+      while (i < cstr.length) { 
+        hash(cstr.data(i)) = i
+        i += 1
+      } 
+    }
+    hash
+  }
+  
+  def --> (b:Dict):IMat = { 
+    val bhash = b.makeHash
+    val out = IMat(length, 1)
+    var i = 0
+    while (i < length) { 
+      if (bhash.contains(cstr(i))) { 
+        out.data(i) = bhash(cstr(i))
+      } else { 
+        out.data(i) = -1
+      }
+      i += 1
+    }
+    out
+  }
+
+}
+
+object Dict {
+  
+  def apply(cstr:CSMat, counts:IMat):Dict = {
+    val out = new Dict(cstr)
+    out.counts = counts
+    out
+  }
+  
+  def apply(cstr:CSMat, counts:IMat, h:HashMap[String,Int]):Dict = {
+    val out = new Dict(cstr)
+    out.counts = counts
+    out.hash = h
+    out
+  }
+    
+  def _union(dicts:Dict*):HashMap[String, Int] = {
+    val hash = new HashMap[String, Int]()
+    var nelems = 0
+    dicts.foreach((d:Dict) => {
+    	var i = 0 
+    	while (i < d.length) {
+    		if (! hash.contains(d.cstr(i))) {
+    			hash(d.cstr(i)) = nelems
+    			nelems += 1
+    		}
+    		i += 1
+    	}
+    })
+    hash
+  }
+  
+  def getCSMat(h:HashMap[String,Int]):CSMat = {
+    val out = CSMat(h.size, 1)
+    h.foreach{case (s:String, i:Int) => out.data(i) = s}
+    out
+  }
+    
+  def union(d1: Dict):Dict = {
+    val h = _union(d1)
+    val d = Dict(getCSMat(h), null, h)
+    val d1d = d1 --> d
+    d.counts = accum(d1d, d1.counts, d.length, 1)
+    d
+  }
+  
+  def union(d1: Dict, d2:Dict):(Dict, IMat, IMat) = {
+  	val h = _union(d1, d2)
+    val d = Dict(getCSMat(h), null, h)
+    val d1d = d1 --> d
+    val d2d = d2 --> d
+    d.counts = accum(d1d, d1.counts, d.length, 1) +
+               accum(d2d, d2.counts, d.length, 1);
+    (d, d1d, d2d)
+  }
+
+  def union(d1: Dict, d2:Dict, d3:Dict):(Dict, IMat, IMat, IMat) = {
+  	val h = _union(d1, d2, d3)
+    val d = Dict(getCSMat(h), null, h)
+    val d1d = d1 --> d
+    val d2d = d2 --> d
+    val d3d = d3 --> d
+    d.counts = accum(d1d, d1.counts, d.length, 1) +
+               accum(d2d, d2.counts, d.length, 1) +
+               accum(d3d, d3.counts, d.length, 1);
+    (d, d1d, d2d, d3d)
+  }
+  
+  def union(d1: Dict, d2:Dict, d3:Dict, d4:Dict):(Dict, IMat, IMat, IMat, IMat) = {
+    val h = _union(d1, d2, d3, d4)
+    val d = Dict(getCSMat(h), null, h)
+    val d1d = d1 --> d
+    val d2d = d2 --> d
+    val d3d = d3 --> d
+    val d4d = d4 --> d
+    d.counts = accum(d1d, d1.counts, d.length, 1) +
+               accum(d2d, d2.counts, d.length, 1) +
+               accum(d3d, d3.counts, d.length, 1) +
+               accum(d4d, d4.counts, d.length, 1);
+    (d, d1d, d2d, d3d, d4d)
+  }
+  
+  def union(d1: Dict, d2:Dict, d3:Dict, d4:Dict, d5:Dict):(Dict, IMat, IMat, IMat, IMat, IMat) = {
+    val h = _union(d1, d2, d3, d4, d5)
+    val d = Dict(getCSMat(h), null, h)
+    val d1d = d1 --> d
+    val d2d = d2 --> d
+    val d3d = d3 --> d
+    val d4d = d4 --> d
+    val d5d = d5 --> d
+    d.counts = accum(d1d, d1.counts, d.length, 1) +
+               accum(d2d, d2.counts, d.length, 1) +
+               accum(d3d, d3.counts, d.length, 1) +
+               accum(d4d, d4.counts, d.length, 1) +
+               accum(d5d, d5.counts, d.length, 1);
+    (d, d1d, d2d, d3d, d4d, d5d)
+  }
+
+}
