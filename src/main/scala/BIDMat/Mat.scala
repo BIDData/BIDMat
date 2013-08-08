37ca11394c5f7618ec2c81faf6feca59f90ec499
jcanny <jfc@cs.berkeley.edu>
2013-08-07 20:00:42 -0700

    DDS problem

--- src/main/scala/BIDMat/Mat.scala
+++ src/main/scala/BIDMat/Mat.scala
@@ -227,17 +227,16 @@ object Mat {
 
   final val myrand = new java.util.Random(MSEED)
   
-  val _cache2 = HashMap.empty[Tuple2[Long,Int], SoftReference[Mat]]
+  val _cache2 = HashMap.empty[Tuple2[Long,Int], Mat]
   
-  val _cache3 = HashMap.empty[Tuple3[Long,Long,Int], SoftReference[Mat]]
+  val _cache3 = HashMap.empty[Tuple3[Long,Long,Int], Mat]
   
-  val _cache4 = HashMap.empty[Tuple4[Long,Long,Long,Int], SoftReference[Mat]]
+  val _cache4 = HashMap.empty[Tuple4[Long,Long,Long,Int], Mat]
   
   def cache2(key:Tuple2[Long,Int]):Mat = {
     _cache2.synchronized {
     	if (_cache2.contains(key)) {
-    		val rr = _cache2(key)
-    		if (rr != null) rr.get else null
+    		_cache2(key)
     	} else {
     		null
     	}
@@ -247,8 +246,7 @@ object Mat {
   def cache3(key:Tuple3[Long,Long,Int]):Mat = {
     _cache3.synchronized {
     	if (_cache3.contains(key)) {
-    		val rr = _cache3(key)
-    		if (rr != null) rr.get else null
+    		_cache3(key)
     	} else {
     		null
     	}
@@ -259,8 +257,7 @@ object Mat {
   def cache4(key:Tuple4[Long,Long,Long,Int]):Mat = {
     _cache4.synchronized {
     	if (_cache4.contains(key)) {
-    		val rr = _cache4(key)
-    		if (rr != null) rr.get else null
+    		_cache4(key)
     	} else {
     		null
     	}
@@ -269,19 +266,19 @@ object Mat {
   
   def cache2put(key:Tuple2[Long,Int], m:Mat):Unit = {
     _cache2.synchronized {
-    	_cache2(key) = new SoftReference(m)
+    	_cache2(key) = m
     }
   }
   
   def cache3put(key:Tuple3[Long,Long,Int], m:Mat):Unit = {
   	_cache3.synchronized {
-  		_cache3(key) = new SoftReference(m)
+  		_cache3(key) = m
   	}
   }
   
   def cache4put(key:Tuple4[Long,Long,Long,Int], m:Mat):Unit = {
   	_cache4.synchronized {
-  		_cache4(key) = new SoftReference(m)
+  		_cache4(key) = m
   	}
   }
   
