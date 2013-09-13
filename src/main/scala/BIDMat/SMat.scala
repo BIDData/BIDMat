8197347f2dc87b15b35f78f5200cd9a1037ef58d
jcanny <jfc@cs.berkeley.edu>
2013-09-13 11:59:04 -0700

    spair ^* missing

--- src/main/scala/BIDMat/SMat.scala
+++ src/main/scala/BIDMat/SMat.scala
@@ -132,7 +132,7 @@ case class SMat(nr:Int, nc:Int, nnz1:Int, ir0:Array[Int], jc0:Array[Int], data0:
   	  	    if (dd.ncols == 1) {
   					// Seg faults in linux and windows - fixed to use one thread	
   	  	      setnumthreads(1)
-  	  	    	scscmv("N", nrows, ncols, 1.0f, "GLNF", data, ir, jc, dd.data, 0f, out.data)
+  	  	    	scscmv("N", nrows, ncols, 1.0f, "GLNF", data, ir0, jc0, dd.data, 0f, out.data)
   	  	    	setnumthreads(Mat.numOMPthreads)
   	  	    } else {
   	  	    	scscmm("N", nrows, nc, ncols, 1.0f, "GLNF", data, ir0, jc0, dd.data, ncols, 0f, out.data, out.nrows)
