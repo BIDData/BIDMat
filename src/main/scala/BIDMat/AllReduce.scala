9acbbff4fb1dfde7e74dc443d2a42e07bca9d575
jcanny <jfc@cs.berkeley.edu>
2013-10-05 17:10:52 -0700

    allreduce debugged

--- src/main/scala/BIDMat/AllReduce.scala
+++ src/main/scala/BIDMat/AllReduce.scala
@@ -5,18 +5,38 @@ import edu.berkeley.bid.comm.AllReduce
 import scala.actors.Actor._
 import BIDMat.MatFunctions._
 import BIDMat.SciFunctions._
-
+import java.util.concurrent.CountDownLatch
 
 
 object AllReducer {
   
+  def main(args:Array[String]) = {
+    val n = args(0).toInt
+    val dens = args(1).toFloat
+    val ks = args(2).split(",")
+    val allks = IMat(ks.length, 1)
+    for (i <- 0 until ks.length) allks(i) = ks(i).toInt
+    test1(n, dens, allks)
+  }
+  
+  def test1(n:Int, dens:Float, allks:IMat) {
+    val a = sprand(n, n, dens)
+    val dd = spdiag(n)
+    val ad = a + dd
+    val b = rand(n, 1)
+    makeSim(ad, b, allks)
+  }
+  
   def makeSim(a:SMat, b:FMat, allks:IMat) = {
     val M = allks.data.reduceLeft(_*_)
-    val bufsize = 1024*1024;
+    val bufsize = 5 * (a.nnz / M) 
+    val ioff = Mat.ioneBased
     val network = new AllReduce(M)
     val rowvecs = new Array[IMat](M)
     val colvecs = new Array[IMat](M)
     val vvecs = new Array[FMat](M)
+    val irows = new Array[IVec](M)
+    val icols = new Array[IVec](M)
     val smats = new Array[SMat](M)
     val (ii, jj, vv) = find3(a)
     val rr = IMat(rand(ii.nrows, 1)*M)
@@ -25,7 +45,7 @@ object AllReducer {
       rowvecs(i) = IMat(counts(i), 1)
       colvecs(i) = IMat(counts(i), 1)
       vvecs(i) = FMat(counts(i), 1)
-      network.simNetwork(i) = new network.Machine(a.nrows, allks.data, i, M, bufsize, false)
+      network.simNetwork(i) = new network.Machine(a.nrows, allks.data, i, M, bufsize, true)
     }
     var i = 0
     counts.clear
@@ -39,15 +59,21 @@ object AllReducer {
       i += 1
     }
     for (i <- 0 until M) {
-    	smats(i) = new SMat(a.nrows, a.ncols, rowvecs(i).length, SparseMat.incInds(rowvecs(i).data), 
-    			SparseMat.compressInds(new Array[Int](a.ncols+1), a.ncols, colvecs(i).data, rowvecs(i).length), vvecs(i).data)
+      val s = new SMat(a.nrows, a.ncols, rowvecs(i).length, SparseMat.incInds(rowvecs(i).data),
+          SparseMat.compressInds(colvecs(i).data, a.ncols, new Array[Int](a.ncols+1), rowvecs(i).length), vvecs(i).data)
+      irows(i) = new IVec(find(sum(s,2)).data)
+      icols(i) = new IVec(find(sum(s,1)).data)      
     }
+    val latch = new CountDownLatch(M)
     for (i <- 0 until M) {
       actor {
-        network.simNetwork(i).config(new IVec(rowvecs(i).data), new IVec(colvecs(i).data))
+        network.simNetwork(i).config(irows(i), icols(i))
         network.simNetwork(i).reduce(new Vec(vvecs(i).data))
+        latch.countDown()
       }
     }
+    latch.await();
+    println("All done")
   }
   
   
