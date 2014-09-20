32f524522e5363b28c518beef148c2c4369e8c1d
jcanny <jfc@cs.berkeley.edu>
2014-09-20 15:58:09 -0700

    added license file

--- src/main/scala/BIDMat/Mat.scala
+++ src/main/scala/BIDMat/Mat.scala
@@ -261,7 +261,7 @@ abstract class Pair {
 
 object Mat {
   import Ordered._
-  import jline.TerminalFactory
+  import scala.tools.jline.TerminalFactory
   
   var useCache = false						 // Use matrix caching
   
