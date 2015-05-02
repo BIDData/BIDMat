c782f43d6330330176dd40fba406132e1207c7b6
Kieren James-Lubin <kjameslubin@stout.cs.berkeley.edu>
2015-05-02 16:53:06 -0700

    hate to push broken code,  but should checkpoint. fixed many issues with making TMat generic but haven't finished.

--- src/main/scala/BIDMat/TMat.scala
+++ src/main/scala/BIDMat/TMat.scala
@@ -9,29 +9,30 @@ import java.util.Arrays
 import java.util.Comparator
 import scala.math._
 import scala.reflect._
+import scala.math.Numeric._
 
 /*
  * TMat's are composed of vertical tiles, given as an Array of (Dense)Mats.  
  * This allows us to use a different multiplication strategy than for DenseMats.
  *
  * x(i) is the Mat.oneBased col index of the top left corner of tile i.
- * y(i) is the Mat.oneBased row index of the top left corent of tile i.
+ * y(i) is the Mat.oneBased row index of the top left corner of tile i.
  *
- * y should be passed in sorted, and contiguous sections of x with the same
+ * y should be passed in sorted, and contiguous x's with the same
  * y value should also be sorted
  *
  * Does not yet check for overlaps!
  * 
- * if implemented..
- *     width(i) is the width of the i-th tile.
- *     height(i) is the height of the i-th tile.
- * doesn't seem strictly necessary as data has this information
  * 
  * tiles(i) is the i-th tile (as an FMat for now - need to change to Mat)
  * 
  */
 
-class TMat (nr: Int, nc: Int, var x : Array[Int], var y : Array[Int], tiles : Array[FMat]) extends Mat(nr, nc) {
+
+
+class TMat[@specialized(Double,Float,Int,Byte,Long) T] 
+      (nr: Int, nc: Int, var x : Array[Int], var y : Array[Int], tiles : Array[Mat]) 
+      (implicit manifest:ClassTag[T], numeric:Numeric[T]) extends Mat(nr, nc) {
   require(x.length == y.length, "x.length must equal y.length")
   require(x.length == tiles.length, "x.length must equal tiles.length")
   
@@ -65,13 +66,17 @@ class TMat (nr: Int, nc: Int, var x : Array[Int], var y : Array[Int], tiles : Ar
 
        while (rindex < y.length && y(rindex) <= r) {
          while (cindex < x.length && x(cindex) <= c) { 
-	  if ( (x(cindex) + tiles(cindex).ncols > c) && (c-x(cindex) < tiles(cindex).ncols) && (r-y(cindex) < tiles(cindex).nrows) && (y(cindex) <= r) ) {
-           ret = tiles(cindex).apply(r-y(cindex),c-x(cindex)) 
-           found = true
-           return ret
-	  }
-           cindex += 1
-	 }
+          tiles(cindex) match {
+	   case denseMat : DenseMat[T] =>           
+                        if ( (x(cindex) + denseMat.ncols > c) && (c-x(cindex) < denseMat.ncols) && (r-y(cindex) < denseMat.nrows) && (y(cindex) <= r) ) {
+	 
+                          found = true
+                          return ret
+	                }  else {  
+                           cindex += 1 
+                        }
+           }                        
+ 	 }
         cindex = rindex	
         rindex += 1
        }
@@ -83,62 +88,72 @@ class TMat (nr: Int, nc: Int, var x : Array[Int], var y : Array[Int], tiles : Ar
     }
   }
 
-  def full(mat:Mat): FMat  = {
-    val out = FMat.newOrCheckFMat(nrows, ncols, mat)
+  def full(mat:Mat):DenseMat[T]  = {
+    val out = DenseMat.newOrCheck[T](nrows, ncols, mat, GUID, "full".hashCode)
 
     var i = 0
-    var m = tiles(0)
+    var m = tiles(0)    
 
     while (i < y.length) {
-     m = tiles(i)
-     val r:IMat = IMat(m.nrows,1,(y(i) to y(i)+m.nrows).toArray)
-     val c:IMat = IMat(m.ncols,1,(x(i) to x(i)+m.ncols).toArray)
+     tiles(i) match {
+       case denseMat : DenseMat[T] =>          
+                val rowInds:IMat = IMat(denseMat.nrows,1,(y(i) to y(i)+denseMat.nrows).toArray)
+                val colInds:IMat = IMat(denseMat.ncols,1,(x(i) to x(i)+denseMat.ncols).toArray)
 
-     out._update(r,c,m)
-     i += 1
+                out.update(rowInds,colInds,denseMat)
+                i += 1
+        }
     }
     out
   }
 
 /*
- * tMult takes advantage of the tiled structure of TFMat
+ * tMult takes advantage of the tiled structure of TMat
  * 
  * We call tileMult repeatedly
  * tileMult(nr:Int, nc:Int, kk:Int, aroff:Int, acoff:Int, b:FMat, broff:Int, bcoff:Int, c:FMat, croff:Int, ccoff:Int)
  * 
+ * Result is a DenseMat[T]
+ **
  */ 
  
-def tMult(a:FMat, outmat:Mat) : FMat =  {
+def tDMult(a:DenseMat[T], outmat:Mat) : DenseMat[T] =  {
       if (ncols == 1 && nrows == 1){ // left scalar multiply
-  		val out = FMat.newOrCheckFMat(a.nrows, a.ncols, outmat, GUID, a.GUID, "dMult".##)
+  		val out = DenseMat.newOrCheck[T](a.nrows, a.ncols, outmat, GUID, a.GUID, "tdMult".##)
   		Mat.nflops += a.length
  
 		var i = 0
-  		val dvar = tiles(0).data(0)
-  		while (i < a.length) {
-  			out.data(i) = dvar * a.data(i)
-  			i += 1
-  		}	
+
+                tiles(0) match { 
+                   case denseMat : DenseMat[T] => 
+                       val dvar = denseMat.data(0)
+  		
+                       while (i < a.length) {
+                                out.data(i) = numeric.times(dvar,a.data(i))
+         	         	i += 1
+   	         	}	
+                }
   		out			  
-      } else if (a.ncols == 1 && a.nrows == 1){ // right scalar multiply. should refactor to return a TFMat later
-                val out = FMat.newOrCheckFMat(nrows, ncols, outmat, GUID, a.GUID, "dMult".##)
+      } else if (a.ncols == 1 && a.nrows == 1){ 
+                val out = DenseMat.newOrCheck[T](nrows, ncols, outmat, GUID, a.GUID, "tdMult".##)
   		
   		var i = 0
   		val dvar = a.data(0)
-                var m = tiles(0)
 
   		while (i < tiles.length) {
-                        m = tiles(i)
-                        val r:IMat = IMat(m.nrows,1,(y(i) to y(i)+m.nrows).toArray)
-                        val c:IMat = IMat(m.ncols,1,(x(i) to x(i)+m.ncols).toArray)
+                        tiles(i) match {
+                           case denseMat : DenseMat[T] =>
+		               val rowInds : IMat = IMat(denseMat.nrows,1,(y(i) to y(i)+denseMat.nrows).toArray)
+                               val colInds : IMat = IMat(denseMat.ncols,1,(x(i) to x(i)+denseMat.ncols).toArray)
+  			       out.update(rowInds,colInds,denseMat.fDMult(a,out(rowInds,colInds)))
 
-  			out._update(r,c,m.fDMult(a,out(r,c)))
-  			i += 1
+                	   }
+        	      	i += 1
   		}
  
   		out
       } else if (ncols == a.nrows) {
-  		var out = FMat.newOrCheckFMat(nrows, a.ncols, outmat, GUID, a.GUID, "dMult".##)
+  		var out = FMat.newOrCheckFMat(nrows, a.ncols, outmat, GUID, a.GUID, "tMult".##)
                 var tmp = FMat.newOrCheckFMat(nrows, a.ncols, null)
 
                 var i = 0
@@ -152,11 +167,12 @@ def tMult(a:FMat, outmat:Mat) : FMat =  {
                     // not sure
                     out 
 		  } else {
-                       // println("y(i): " + y(i))
-                       // println("x(i): " + x(i))
-                       out += m.tileMult(m.nrows, a.ncols, m.ncols, 0, 0, a, x(i), 0, tmp, y(i), 0)
-                       // println("tmp:\n" + tmp)
-                       // println("out:\n" + out)
+		       m match {
+                       case fMat : FMat =>
+                            fMat.tileMult(fMat.nrows, a.ncols, fMat.ncols, 0, 0, a, x(i), 0, tmp, y(i), 0)
+                            tmp.ggMatOp(out,Mop_Plus, out)
+                       }
+
   		  }
                  i+= 1  			 
 		} 
