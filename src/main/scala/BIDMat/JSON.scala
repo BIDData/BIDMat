ea902c81d28c0c63f33399204ef848c5e67cd1a7
John Canny <jfc@cs.berkeley.edu>
2015-10-01 16:58:29 -0700

    added pretty JSON

---
+++ src/main/scala/BIDMat/JSON.scala
@@ -0,0 +1,27 @@
+
+package BIDMat
+import java.util.Map;
+import com.cedarsoftware.util.io.JsonWriter;
+import com.cedarsoftware.util.io.JsonReader;
+
+
+object JSON {
+  import java.util.Map;
+  import java.util.HashMap;;
+  
+  val jopts:Map[String,AnyRef] = new HashMap;
+  jopts.put(JsonWriter.PRETTY_PRINT, java.lang.Boolean.TRUE);
+ // jopts(JsonWriter.PRETTY_PRINT) = java.lang.Boolean.TRUE;
+    
+  def toJSON(obj:AnyRef):String = JsonWriter.objectToJson(obj);
+  
+  def toJSON(obj:AnyRef, pretty:Boolean):String = {
+    if (pretty) {
+    	JsonWriter.objectToJson(obj, jopts);      
+    } else {
+    	JsonWriter.objectToJson(obj);
+    }
+  }
+  
+  def fromJSON(str:String):AnyRef = JsonReader.jsonToJava(str)
+}
---
+++ src/main/scala/BIDMat/Options.scala
@@ -0,0 +1,48 @@
+
+package BIDMat
+import java.lang.reflect.Method
+import scala.util.Sorting._
+
+class about(x:String) extends scala.annotation.Annotation {}
+
+trait Opts {
+  def what:Unit = {
+    val ignore = List("what", "wait", "equals", "toString", "hashCode", "getClass", "notify", "notifyAll", "ignore", "copyFrom")
+
+    val a = for (meth <- this.getClass.getMethods 
+    		if (!meth.getName.contains("$eq") && !meth.getName.contains("$$methOrdering") && !ignore.contains(meth.getName))) yield meth;
+    implicit object methOrdering extends Ordering[Method] { 
+    	def compare(x: Method, y: Method) = x.getName.toLowerCase.compareTo(y.getName.toLowerCase);
+    }
+    stableSort(a);
+    println("Option Name       Type          Value");
+    println("===========       ====          =====");
+    for (meth <- a) {
+      val cname = meth.getDeclaringClass().getName
+      val params = meth.getParameterTypes();
+      if (params.length == 0) {
+        val ref = meth.invoke(this)
+        val valstring = if (ref != null) ref.toString else "null"
+        val valstr = valstring.substring(0,math.min(20, valstring.length)) + (if (valstring.length > 20) "..." else "")
+        println("%-16s  %-12s  %s" format (meth.getName, meth.getReturnType.getSimpleName, valstr))
+      }
+    }    
+  }
+  
+  def copyFrom(that:Opts) {
+    val ignore = List("what", "wait", "equals", "toString", "hashCode", "getClass", "notify", "notifyAll", "ignore", "copyFrom");
+    val methods = for (meth <- this.getClass.getMethods if (meth.getName.contains("$eq") && !ignore.contains(meth.getName))) yield meth;
+    for (setter <- methods) {
+      val setname = setter.getName;
+      val getname = setname.substring(0, setname.length - 4);  // getter name = remove _= suffix from a setter method
+      try {
+    	  val getter = that.getClass.getMethod(getname);         // Try to find a method with that name in the other class
+    	  val value = getter.invoke(that);                       // Get the value from that
+    	  setter.invoke(this, value);                            // Copy into this
+      } catch {
+        case _:NoSuchMethodException => {}                     // OK if not found. "that" could be a superclass
+      }
+    }
+  }
+}
+
