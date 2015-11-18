5c4f7ed945d7d7aac34f8fa2544258e40a7c1568
John Canny <jfc@cs.berkeley.edu>
2015-11-17 16:26:12 -0800

    added HDFSIO.scala

--- src/main/scala/BIDMat/HMat.scala
+++ src/main/scala/BIDMat/HMat.scala
@@ -78,17 +78,6 @@ case class HMat(nr:Int, nc:Int, fileList:List[String], varname:String, blkinds:A
   }
 }
 
-trait HDFSIOtrait {
-  def writeMat(name:String, Mat:Mat, comp:Int)
-  def writeND(name:String, nd:ND, comp:Int)
-  def writeMats(name:String, Mats:Array[Mat], comp:Int)
-  def writeNDs(name:String, nds:Array[ND], comp:Int)
-  def readMat(name:String, omat:Mat):Mat
-  def readND(name:String, omat:ND):ND
-  def readMats(name:String, omats:Array[Mat]):Array[Mat]
-  def readNDs(name:String, omat:Array[ND]):Array[ND]
-}
-
 object HMat {
   
   val byteOrder = ByteOrder.LITTLE_ENDIAN
