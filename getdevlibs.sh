bc3de2e9dcae3604d65cc0fb20947ded43b82e60
jcanny <jfc@cs.berkeley.edu>
2016-01-30 15:48:48 -0800

    getdevlibs for arm

--- getdevlibs.sh
+++ getdevlibs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ "${ARCH}" = "" ];then
+    ARCH=`arch`
+fi
+
 BIDMAT_ROOT="${BASH_SOURCE[0]}"
 if [ ! `uname` = "Darwin" ]; then
   BIDMAT_ROOT=`readlink -f "${BIDMAT_ROOT}"`
@@ -23,8 +27,13 @@ elif [ "$OS" = "Windows_NT" ]; then
     subdir="win"
     curl -o liblist.txt ${source}/lib/liblist_win.txt
 else
-    subdir="linux"
-    curl -o liblist.txt ${source}/lib/liblist_linux.txt
+    if [[ "${ARCH}" == arm* ]]; then
+        subdir="linux_arm"
+        curl -o liblist.txt ${source}/lib/liblist_linux_arm.txt
+    else
+        subdir="linux"
+        curl -o liblist.txt ${source}/lib/liblist_linux.txt
+    fi
 fi
 curl -o exelist.txt ${source}/lib/exelist.txt
 
