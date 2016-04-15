63ed31822d026dbb5fc6fbe1e594fc3df7086a26
Philip Hayes <philiphayes9@gmail.com>
2016-04-14 22:12:54 -0700

    Small fixes to OpenCL tests and device loading

--- src/test/scala/BIDMat/BIDMatSpec.scala
+++ src/test/scala/BIDMat/BIDMatSpec.scala
@@ -6,7 +6,7 @@ abstract class BIDMatSpec extends FlatSpec
   with Matchers
   with BeforeAndAfterAll {
 
-  def assert_approx_eq(a: Array[Float], b: Array[Float], eps: Float = 1e-5f) = {
+  def assert_approx_eq(a: Array[Float], b: Array[Float], eps: Float = 1e-4f) = {
     (a, b).zipped foreach {
       case (x, y) => x should equal (y +- eps)
     }
